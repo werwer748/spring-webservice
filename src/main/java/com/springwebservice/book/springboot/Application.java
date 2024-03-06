aa4e3d5ed0fd8c96dd81ea539b9101da72ce683d
hugo <werwer748@naver.com>
2024-03-06 21:47:17 +0900

    1. 컨트롤러에 api 만들어보기 2. 컨트롤러 테스트코드 작성해보기 3. Dto 작성해보기 4. Dto 테스트코드 작성해보기

---
+++ src/main/java/com/springwebservice/book/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.springwebservice.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정
+// - 이 어노테이션이 있는 위치부터 설정을 읽어가기 떄문에 항상 프로젝트의 최상단에 위치해야 한다.
+public class Application { // 프로젝트의 메인 클래스
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
