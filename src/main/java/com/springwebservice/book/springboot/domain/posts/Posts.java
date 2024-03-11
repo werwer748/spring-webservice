39da93607cbae1542c4f3ffe2782157928450a83
hugo <werwer748@naver.com>
2024-03-11 23:59:07 +0900

    1. JPA 사용해보기 2. 도메인패턴? 3. 레포지토리 설명 4. 테스트 sql문 확인 설정 추가 - application.properties

---
+++ src/main/java/com/springwebservice/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,36 @@
+package com.springwebservice.book.springboot.domain.posts;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor // 기본 생성자 추가 public Posts() {} < 파라미터가없는..
+@Entity // 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍으로 테이블 이름을 매칭
+public class Posts {
+
+    @Id // 테이블의 pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // pk 생성 규칙
+    private Long id;
+
+    @Column(length = 500, nullable = false) // 기본설정을 바꿔야 되는 경우 선언, 설정을 명시하고 싶은 경우
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    /**
+     * Builder?
+     * 해당 클래스의 빌더 패턴 클래스를 생성
+     * 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
+     */
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
---
+++ src/test/java/com/springwebservice/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.springwebservice.book.springboot.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        /**
+         * save
+         * posts 테이블에 insert/update 쿼리를 실행함.
+         * id 값이 있다면 update, 없다면 insert 쿼리가 실행 됨.
+         */
+        postsRepository.save(Posts.builder()
+                        .title(title)
+                        .content(content)
+                        .author("werwer748@naver.com")
+                        .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
